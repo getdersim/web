691daa8e8557dd1219a0244f03957a5e5685c80b
cagataycali <cagataycali@icloud.com>
2018-01-21 06:25:31 +0300

    ✨  Generate gif and thumbnail

--- hooks/index.js
+++ hooks/index.js
@@ -24,6 +24,28 @@ const generateGIF = url => {
     })
   })
 }
+const generateThumbnail = url => {
+  return new Promise(async (resolve, reject) => {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: true
+    })
+    const page = await browser.newPage()
+    await page.goto(`http://localhost:5000/thumbnail.html?pdf=${url}`)
+
+    page.on('console', async msg => {
+      var data = msg._text.replace(/^data:image\/\w+;base64,/, '')
+      var buffer = Buffer.from(data, 'base64')
+      let d = new Date()
+      let filename = `${d.valueOf()}.png`
+      fs.writeFile(filename, buffer)
+      await browser.close()
+      resolve(filename)
+    })
+  })
+}
+
+// generateThumbnail('https://firebasestorage.googleapis.com/v0/b/getdersim.appspot.com/o/document%2FAlx6G918rubxhj6yegR6WlFWiay1%2F1516504221992.pdf?alt=media&token=cafc38ba-e30c-4da7-8715-fc026d649b41').then(img => console.log(img))
 //
-// generateGIF('https://firebasestorage.googleapis.com/v0/b/dersim-app.appspot.com/o/dokuman%2F1ytpa6J5KjXNES6JFFjtaKWvoLF2%2F1516460218962.pdf?alt=media&token=f7184e1c-2089-49d4-966b-00ede78c98b3')
+// generateGIF('https://firebasestorage.googleapis.com/v0/b/getdersim.appspot.com/o/document%2FAlx6G918rubxhj6yegR6WlFWiay1%2F1516504221992.pdf?alt=media&token=cafc38ba-e30c-4da7-8715-fc026d649b41')
 //   .then(filename => console.log(filename))
